919bf1abd1a5dbbcb89de0da386d127692f9ed28
ivanna <117600062+ivannamal@users.noreply.github.com>
2025-10-15 23:26:28 +0300

    added NameSection

---
+++ src/components/NameSection/NameSection.jsx
@@ -0,0 +1,11 @@
+import "./NameSection.scss";
+
+export default function NameSection() {
+    return (
+        <section className="name_sec" id="home">
+                <h1 className="name__title">
+                    NuHz Team
+                </h1>
+        </section>
+    );
+}
