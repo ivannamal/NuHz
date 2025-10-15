48f242580c13f61e5e09720ef1446f547a5bd4ff
Ann Kuts <kuutsann@gmail.com>
2025-10-15 22:28:55 +0300

    feat: add TeamCard and TeamSection components with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Navigation/Header/Header';
+import TeamSection from "./components/TeamSection/TeamSection.jsx";
 import Footer from './components/Navigation/Footer/Footer'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <TeamSection />
       <Footer />
     </>
   )
---
+++ src/components/TeamSection/TeamSection.jsx
@@ -0,0 +1,33 @@
+import avatarAnna from "../../assets/avatars/AnnKuts.png";
+import avatarMariia from "../../assets/avatars/Impe11e.png";
+import avatarIvanna from "../../assets/avatars/ivannamal.png";
+import avatarVladyslav from "../../assets/avatars/taitami.png";
+import TeamCard from "./TeamCard/TeamCard.jsx";
+import "./TeamSection.css";
+const TeamSection = () => {
+  const teamMembers = [
+    {avatar: avatarAnna, name: "Anna", role: "Teamlead | FullStack developer", bio: "Honest, purposeful, perseverant, fast learning, sociable, highly motivated, reliable"},
+    {avatar: avatarMariia, name: "Mariia", role: "Backend developer | Fullstack developer", bio: "Responsible<br />Sociable<br />Analytical thinking"},
+    {avatar: avatarIvanna, name: "Ivanna", role: "Frontend Developer", bio: "Сommunicative, teamworking, problem-solver, adaptive, loves critical thinking, takes leadership, and love chebupeli"},
+    {avatar: avatarVladyslav, name: "Vladyslav", role: "Backend Developer", bio: "beginner future millionaire<br />eze internship at gallera in dreams<br />lowlife in past life (10 days ago)"},
+  ];
+
+  return (
+    <section id="team" className="team-section">
+      <h2>Our Team</h2>
+      <div className="team-cards">
+        {teamMembers.map((member) => (
+          <TeamCard
+            key={member.name}
+            avatar={member.avatar}
+            name={member.name}
+            role={member.role}
+            bio={member.bio}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default TeamSection;
