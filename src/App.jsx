d1e09bc0e85ddd9189c15e5a75d81d6177373009
ivannamal <imalaschuk@gmail.com>
2025-10-15 22:31:38 +0300

    added name (section1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from './components/Navigation/Header/Header';
 import Footer from './components/Navigation/Footer/Footer'
-
+import NameSection from "./components/Hero/NameSection.jsx";
 function App() {
 
   return (
     <>
       <Header />
+        <NameSection />
       <Footer />
     </>
   )
