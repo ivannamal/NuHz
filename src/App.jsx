35861a5f50bb4983332a11d4459a654fac4aa791
ivanna <117600062+ivannamal@users.noreply.github.com>
2025-10-15 23:27:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from './components/Navigation/Header/Header';
 import Footer from './components/Navigation/Footer/Footer'
-
+import NameSection from "./components/NameSection/NameSection.jsx";
 function App() {
 
   return (
     <>
       <Header />
+        <NameSection />
       <Footer />
     </>
   )
